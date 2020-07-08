546536098966da4750dc1401a3b95cd6ed022152
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-07-08 21:09:14 +0530

    initial structure

---
+++ Assignment1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public static List<Product> list = new List<Product>
+        {
+            new Product(){ pid=0001,pname="Laptop", price=50000.0,stock=5},
+            new Product() { pid=0002,pname="Phone", price=10000.50, stock=10}
+        };
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            list.Add(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Assignment1/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {
+        public int pid { get; set; }
+        public string pname { get; set; }
+        public double price { get; set; }
+        public int stock { get; set; }
+
+    }
+}
